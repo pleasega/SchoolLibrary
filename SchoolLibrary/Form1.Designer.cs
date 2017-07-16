0fb7bf8b774fdb078c6a82f8a7e4ad3b90587a2e
pleasega <pleasega@gmail.com>
2017-07-16 21:40:13 +0800

    Include version number and added x86 build

--- SchoolLibrary/Form1.Designer.cs
+++ SchoolLibrary/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.borrow_btn = new System.Windows.Forms.Button();
             this.return_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // borrow_btn
@@ -54,17 +55,30 @@
             this.return_btn.UseVisualStyleBackColor = true;
             this.return_btn.Click += new System.EventHandler(this.return_btn_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(398, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "v1.0.0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 231);
+            this.ClientSize = new System.Drawing.Size(442, 245);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.return_btn);
             this.Controls.Add(this.borrow_btn);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "School Library";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +86,7 @@
 
         private System.Windows.Forms.Button borrow_btn;
         private System.Windows.Forms.Button return_btn;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SchoolLibrary/Form1.cs
+++ SchoolLibrary/Form1.cs
@@ -30,5 +30,6 @@ namespace SchoolLibrary
             frmScan.Show();
             this.Hide();
         }
+
     }
 }
